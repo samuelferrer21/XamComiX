56a4eac400a77b425472ad272c3804e382105f64
xammyboii <xavieryabados.09@gmail.com>
2023-10-11 18:51:48 -0500

    created an about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Routes for Comics
   root to: 'comics#index'
   get 'comics/show'
-  resources :comics, only: [:index, :show]
+  # resources :comics, only: [:index, :show]
+
+  # Route for About page
+  get 'comics/about'
 end
