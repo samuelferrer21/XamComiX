404d13549efeb49104b9b40a030850719ddfa8d1
fazerlazer2101 <samferrer145@gmail.com>
2023-10-13 21:03:52 -0500

    Styled index and show for publishers

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,7 +1,10 @@
 class PublishersController < ApplicationController
   def index
     @publishers = Publisher.all.order("founded ASC")
+    @comics = Comic
   end
 
-  def show; end
+  def show
+    @publisher = Publisher.find(params[:id])
+  end
 end
