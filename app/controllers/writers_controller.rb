238fd8595b212b519eba25822c9b1b466e771b4e
xammyboii <xavieryabados.09@gmail.com>
2023-10-17 18:44:49 -0500

    implemented kaminari pagination for writers index

--- app/controllers/writers_controller.rb
+++ app/controllers/writers_controller.rb
@@ -1,8 +1,11 @@
 class WritersController < ApplicationController
+  # GET /writers
   def index
-    @writers = Writer.all.order("first_name ASC")
+    # @writers = Writer.all.order("first_name ASC")
+    @writers = Writer.order(:first_name).page(params[:page]).per(10)
   end
 
+  # GET /writers/:id
   def show
     @writer = Writer.find(params[:id])
   end
