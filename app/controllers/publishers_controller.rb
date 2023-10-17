f96d2f414bf09b1ab2ddfecc555d68113dced5a5
fazerlazer2101 <samferrer145@gmail.com>
2023-10-16 19:36:28 -0500

    Removed old comments

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -6,12 +6,6 @@ class PublishersController < ApplicationController
   def show
     @publisher = Publisher.find(params[:id])
 
-    # if params[:author_id] == nil?
-    #   @comic_result = Publisher.find(params[:id]).comics
-    # else
-    #   Publisher.find(params[:id]).comics
-    # end
-
     @comic_result = if params[:author_id].nil?
                       Publisher.find(params[:id]).comics
                     else
@@ -19,17 +13,6 @@ class PublishersController < ApplicationController
                     end
 
     @id = params[:id]
-    @currentAuthor = if params[:author_id].nil?
-                       "none"
-                     else
-                       params[:author_id]
-                     end
     @writer = Writer.joins(:comics).where("publisher_id == " + params[:id]).distinct
   end
-
-  # def showFilter
-  #   @comic_result = Publisher.find(params[:id]).comics.where("writer_id == #{params[:author_id]}")
-  #   @id = params[:id]
-  #   @writer = Writer.joins(:comics).where("publisher_id == " + params[:id]).distinct
-  # end
 end
